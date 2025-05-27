7057d617169caf976b6c87213d92c4d28c218735
XRHoward <96023172+XRHoward@users.noreply.github.com>
2025-05-27 09:35:36 +0200

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -32,4 +32,13 @@ export default function Header({ globalSettings }) {
           </nav>
           <div className="md:hidden">
             <button className="text-gray-600">
-              <svg className="w-6 h-6" fill="none" stroke="current
+              <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
