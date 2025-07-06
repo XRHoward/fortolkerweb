92b0d986f48008f4b6ec4d22541dae331c0be7e8
XRHoward <96023172+XRHoward@users.noreply.github.com>
2025-07-06 07:40:24 +0200

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,10 @@
 import Header from './Header';
 import Footer from './Footer';
 
-export default function Layout({ children }) {
+export default function Layout({ children, globalSettings }) {
   return (
     <>
-      <Header />
+      <Header globalSettings={globalSettings} />
       <main className="pt-24 md:pt-28 px-4">
         {children}
       </main>
