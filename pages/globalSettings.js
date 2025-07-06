52b68c63d4dda59aed6f6ec9916cd9dc43f5b0a1
XRHoward <96023172+XRHoward@users.noreply.github.com>
2025-07-06 08:14:48 +0200

    Update globalSettings.js

--- pages/globalSettings.js
+++ pages/globalSettings.js
@@ -1,17 +1,13 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import { client, urlFor } from '../lib/sanity';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
 
 export default function GlobalSettingsPage({ globalSettings }) {
   return (
-    <div className="min-h-screen flex flex-col">
+    <>
       <Head>
         <title>{globalSettings?.siteName || 'Globale innstillinger'} - Fortolker AS</title>
       </Head>
-
-      <Header />
 
       <main className="flex-grow py-16">
         <div className="container mx-auto px-4">
@@ -68,9 +64,7 @@ export default function GlobalSettingsPage({ globalSettings }) {
           )}
         </div>
       </main>
-
-      <Footer />
-    </div>
+    </>
   );
 }
 
