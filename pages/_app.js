85b99dd69f72a0316ce49ec9a5be799cb82d22df
XRHoward <96023172+XRHoward@users.noreply.github.com>
2025-07-07 20:55:00 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,9 @@ import Layout from '../components/Layout';
 function MyApp({ Component, pageProps }) {
   return (
     <Layout globalSettings={pageProps.globalSettings}>
+    <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
